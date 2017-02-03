2c6f7a684c186c960f91ba4b1eee7b9fef7f63d7
chestnut <shivam31august1996@gmail.com>
2017-02-03 16:06:10 +0530

    INTERFACE
    THIS SCRIPT CONTAINS THE FUNCTIONS AND READS THE DATA FROM PICKLE FILES.

---
+++ sentiment.py
@@ -0,0 +1,93 @@
+import pickle
+from nltk.classify import ClassifierI
+from statistics import mode
+from nltk.tokenize import word_tokenize
+
+
+class VoteClassifier(ClassifierI):
+    def __init__(self, *classifiers):
+        self._classifiers = classifiers
+
+    def classify(self, features):
+        votes = []
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
+        return mode(votes)
+
+    def confidence(self, features):
+        votes = []
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
+
+        choice_votes = votes.count(mode(votes))
+        conf = choice_votes / len(votes)
+        return conf
+
+#READS THE PICKLED WORD FEATURES AS WELL AS SAVED CLASSIFIERS
+
+documents_f = open("pickled_algos_documents.pickle", "rb")
+documents = pickle.load(documents_f)
+documents_f.close()
+
+word_features5k_f = open("pickled_algos_word_features5k.pickle", "rb")
+word_features = pickle.load(word_features5k_f)
+word_features5k_f.close()
+
+
+def find_features(document):
+    words = word_tokenize(document)
+    features = {}
+    for w in word_features:
+        features[w] = (w in words)
+
+    return features
+
+open_file = open("pickled_algos_originalnaivebayes5k.pickle", "rb")
+classifier = pickle.load(open_file)
+open_file.close()
+
+
+open_file = open("pickled_algos_MNB_classifier5k.pickle", "rb")
+MNB_classifier = pickle.load(open_file)
+open_file.close()
+
+
+
+open_file = open("pickled_algos_BernoulliNB_classifier5k.pickle", "rb")
+BernoulliNB_classifier = pickle.load(open_file)
+open_file.close()
+
+
+open_file = open("pickled_algos_LogisticRegression_classifier5k.pickle", "rb")
+LogisticRegression_classifier = pickle.load(open_file)
+open_file.close()
+
+
+open_file = open("pickled_algos_LinearSVC_classifier5k.pickle", "rb")
+LinearSVC_classifier = pickle.load(open_file)
+open_file.close()
+
+
+open_file = open("pickled_algos_SGDC_classifier5k.pickle", "rb")
+SGDC_classifier = pickle.load(open_file)
+open_file.close()
+
+
+voted_classifier = VoteClassifier(
+                                  classifier,
+                                  LinearSVC_classifier,
+                                  MNB_classifier,
+                                  BernoulliNB_classifier,
+                                  LogisticRegression_classifier)
+
+
+
+def sentiment(text):
+    feats = find_features(text)
+    return voted_classifier.classify(feats),voted_classifier.confidence(feats)
+
+
+
+
